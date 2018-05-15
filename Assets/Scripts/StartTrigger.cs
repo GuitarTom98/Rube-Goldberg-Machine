66e923763e4e8c4b2a21793f46ac4fd2099c5bd7
Tom Udale <s5068741@bournemouth.ac.uk>
2018-05-15 16:39:57 +0100

    inital commit

---
+++ Assets/Scripts/StartTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTrigger : MonoBehaviour {
+
+    public float MaxMoveAmount;
+
+    float movedSoFar = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float amountToMoveThisFrame = 2 * Time.deltaTime;
+        
+        if (movedSoFar < MaxMoveAmount)
+        {
+            transform.Translate(amountToMoveThisFrame, 0, 0);
+            movedSoFar += amountToMoveThisFrame;
+        }
+
+	}
+}
