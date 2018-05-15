cd4135fec47521a96606b87b425aefe944afd90a
Tom Udale <s5068741@bournemouth.ac.uk>
2018-05-15 18:13:24 +0100

    Added a script to make the seesaw delay it's physics

---
+++ Assets/Scripts/DelayRigidbody.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayRigidbody : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "SeesawRotation")
+        {
+            rb.isKinematic = false;
+        }
+        
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
