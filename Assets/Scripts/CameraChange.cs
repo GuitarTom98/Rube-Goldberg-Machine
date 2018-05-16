5a1eac7d78cdf2084522fbb5ef4cb3701a09ee88
Tom Udale <s5068741@bournemouth.ac.uk>
2018-05-16 12:33:12 +0100

    Added the ability to track multiple objects with the camera

---
+++ Assets/Scripts/CameraChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour {
+
+    public static int ChangeCamera = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CameraTrigger") {
+
+            ChangeCamera = ChangeCamera + 1;
+ 
+        }
+    }
+
+
+}
--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,16 +2,41 @@
 
 public class CameraFollow : MonoBehaviour {
 
-    public Transform target;
+    public Transform target1;
+    public Transform target2;
 
     public float smoothSpeed = 5f;
     public Vector3 offset;
+    private int ActiveCam;
 
     private void FixedUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
-        transform.position = smoothedPosition;
+        GameObject Camera = GameObject.Find("Main Camera");
+        CameraChange cameraChange = Camera.GetComponent<CameraChange>();
+        ActiveCam = CameraChange.ChangeCamera;
+
+        if (ActiveCam == 0) {
+
+            Vector3 desiredPosition = target1.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+
+            transform.LookAt(target1);
+
+        }
+
+        if (ActiveCam == 1)
+        {
+
+            Vector3 desiredPosition = target2.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+
+            transform.LookAt(target2);
+
+        }
+
+
     }
 
 
