85b6d413b862c2fc418c9bee22d5f59de2d124d7
Tom Udale <s5068741@bournemouth.ac.uk>
2018-05-18 18:36:18 +0100

    Added the second plank fall

---
+++ Assets/Scripts/PlankFall2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlankFall2 : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    bool inTrigger = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+
+        GameObject FallTrigger = GameObject.Find("PlankFallTrigger2");
+        PlankFallTriggerScript2 plankFallTriggered = FallTrigger.GetComponent<PlankFallTriggerScript2>();
+        inTrigger = PlankFallTriggerScript2.plankFallTriggered;
+
+        if (inTrigger == true)
+        {
+            rb.isKinematic = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlankFallTriggerScript2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlankFallTriggerScript2 : MonoBehaviour {
+
+    public static bool plankFallTriggered = false;
+
+    //Used to determine when the object has entered the trigger zone
+    private void OnTriggerEnter(Collider other)
+    {
+        /*
+        Determines if the object that has entered the zone has 
+        the "PushWall" tag and therefore if it can trigger a the wall push
+        */
+        if (other.gameObject.tag == "Ball")
+        {
+
+            plankFallTriggered = true;
+
+        }
+    }
+}
