2289cca0f0dc9666eaa74411a3c4d67974ca089f
Tom Udale <s5068741@bournemouth.ac.uk>
2018-05-18 19:49:21 +0100

    Final commit of the day

---
+++ Assets/Scripts/DeleteOnCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteOnCollision : MonoBehaviour {
+
+    public GameObject deleteObject;
+
+    public GameObject collideObject;
+
+    void OnCollisionEnter(Collision collision) {
+
+        if (collision.gameObject == collideObject) {
+
+            Destroy(deleteObject);
+
+        }
+
+    }
+
+}
